06885632812c917fb622a2f2b7b6625c19a865fa
Ramona <rare2400@student.miun.com>
2025-05-18 17:03:38 +0200

    tar bort console.log och lägger till kommentarer

--- src/js/search.js
+++ src/js/search.js
@@ -1,13 +1,23 @@
+//fetch searchform to get artist
 const artistSearch = document.getElementById("search-form");
 
+/**
+ * eventlistener for submit that get input-value put into url 
+ * to redirect to the artist-page
+ */
 if (artistSearch) {
     artistSearch.addEventListener('submit', (event) => {
+        /**
+         * prevent page from loading when submitting
+         * @param {SubmitEvent} event - forms submit-event
+         */
         event.preventDefault();
 
+        //variable for the input value
         const artist = document.getElementById("search").value;
-        console.log("sökt artist: ", artist);
 
         if (artist) {
+            //navigate to artist.html with the artists name as parameter in url
             window.location.href = `artist.html?artist=${encodeURIComponent(artist)}`;
         }
     });
